ec66f4a4d13ef2700ded15960435b42d9ca577aa
Lyra-B <glykeriapeppa@gmail.com>
2015-03-10 16:56:21 +0000

    Refactored destination feature to add activity

--- features/add_destination.feature
+++ features/add_destination.feature
@@ -1,5 +1,5 @@
 Feature: Destinations
-@wip @javascript
+@javascript
   Scenario: A traveller adds a destination to their bucketlist
     Given some destinations with some bucketlist items
       And a traveller who has added an item to their bucketlist
@@ -7,3 +7,12 @@ Feature: Destinations
     When I sign in
     When they add a destination
     Then it should appear on the page
+
+@wip @javascript
+  Scenario: A traveller adds an activity to a destination on their bucketlist
+    Given some destinations with some bucketlist items
+      And a traveller who has added an item to their bucketlist
+      And I am on the sign in page
+      And I sign in
+    When they add an activity
+    Then they see their activity under the relevant destination
--- features/step_definitions/destinations_steps.rb
+++ features/step_definitions/destinations_steps.rb
@@ -4,12 +4,29 @@ Given (/^they have signed in$/) do
 end
 
 When(/^they add a destination$/) do
-  fill_in "Name", :with => "India"
-  fill_in "Image URL", :with => "http://example.org/image.jng"
-  click_button "Add Destination"
+  within('#add-destination') do
+    fill_in "Name", :with => "India"
+    fill_in "Image URL", :with => "http://example.org/image.jpg"
+    click_button "Add Destination"
+  end
+end
+
+When(/^they add an activity$/) do
+  within("#destination_#{@italy.id}") do
+    fill_in "Name", :with => "Eat Pasta"
+    fill_in "Image URL", :with => "http://example.org/image.jpg"
+    click_button "Add Activity"
+  end
+end
+
+Then(/^they see their activity under the relevant destination$/) do
+  # binding.pry
+  expect(page.has_content?('Eat Pasta')).to be true
+  expect(page.has_css?("img[src='http://example.org/image.jpg']")).to be true
 end
 
 Then(/^it should appear on the page$/) do
+  # binding.pry
   expect(page.has_content?("India")).to be true
-  expect(page.has_css?("img[src='http://example.org/image.jng']")).to be true
+  expect(page.has_css?("img[src='http://example.org/image.jpg']")).to be true
 end
