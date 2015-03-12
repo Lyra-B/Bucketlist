cd67a6ebd27b7c3549e27c63e8f32925f380022b
Lyra-B <glykeriapeppa@gmail.com>
2015-03-12 15:39:31 +0000

    Added Location to the Activity Form

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -12,6 +12,6 @@ class ActivitiesController < ApplicationController
   end
 
   def allowed_params
-    params.require(:activity).permit(:name, :image_url, :destination_id, :bucket_list_items_attributes => [:traveller_id])
+    params.require(:activity).permit(:name, :image_url, :location, :destination_id, :bucket_list_items_attributes => [:traveller_id])
   end
 end
