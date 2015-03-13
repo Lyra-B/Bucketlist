946ea853641b99c542988af13654ad73d24186d3
Lyra-B <glykeriapeppa@gmail.com>
2015-03-13 09:14:15 +0000

    Attempt to reload the markers after refresh

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,11 @@
 class ActivitiesController < ApplicationController
   def create
     @activity = Activity.create!(allowed_params)
-    binding.pry
     @lats = Activity.all.map{|a| a.latitude}
-    @longs = Activity.all.map{|a| a.longitude}
-    @lat_lng = [@lats, @longs].transpose
+    @lngs = Activity.all.map{|a| a.longitude}
+    @names = Activity.all.map{|a| a.name}
+    @lat_lng_nam = [@lats, @lngs, @names].transpose
+    # binding.pry
     # render @activity
     # render :partial => 'activities/activity', :locals => { :activity => @activity }
     # binding.pry
--- app/controllers/bucket_lists_controller.rb
+++ app/controllers/bucket_lists_controller.rb
@@ -5,6 +5,11 @@ class BucketListsController < ApplicationController
     @activity = Activity.new
     @activities = current_traveller.activities.includes(:destination).order(:destination_id)
     @destination.activities.map{ |a| a.destination }
+    @lats = Activity.all.map{|a| a.latitude}
+    @lngs = Activity.all.map{|a| a.longitude}
+    @names = Activity.all.map{|a| a.name}
+    @lat_lng_nam = [@lats, @lngs, @names].transpose.to_json
+    # binding.pry
     # binding.pry
   end
 end
