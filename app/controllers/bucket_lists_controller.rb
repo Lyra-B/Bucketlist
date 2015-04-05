8b43af89ab10d9fc2e1aff04d13873e4f4d39306
Lyra-B <glykeriapeppa@gmail.com>
2015-04-05 18:04:14 +0100

    Added order by priority as a default

--- app/controllers/bucket_lists_controller.rb
+++ app/controllers/bucket_lists_controller.rb
@@ -3,22 +3,20 @@ class BucketListsController < ApplicationController
     @destination = Destination.new
     @destinations = Destination.all
     @activity = Activity.new
-    @activities = current_traveller.activities.includes(:destination).order(:destination_id)
-    # binding.pry
+    # @activities = current_traveller.activities.includes(:destination).order(:priority)
     @activities_objects = current_traveller.activities.all.to_json(:only=>[:latitude,:longitude, :name, :image_url])
     @destination.activities.map{ |a| a.destination }
-    # @lats = Activity.all.map{|a| a.latitude}
-    # @lngs = Activity.all.map{|a| a.longitude}
-    # @names = Activity.all.map{|a| a.name}
-    # @lat_lng_nam = [@lats, @lngs, @names].transpose.to_json
-    # binding.pry
-    # binding.pry
+    @priorities = Activity.priorities
   end
 
   def order
+    @priorities = Activity.priorities
     @destinations = Destination.includes(:activities)
-    # binding.pry
-    @order = params[:order].downcase.to_sym
+    if params[:order] == "Most Recently Added"
+      @order = "created_at".to_sym
+    else
+      @order = params[:order].downcase.to_sym
+    end
   end
 end
 
