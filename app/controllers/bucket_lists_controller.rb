8d9cd7742860726bb6b3868d34b0e080c1cc0e8d
Lyra-B <glykeriapeppa@gmail.com>
2015-04-06 02:34:27 +0100

    Changed Activities

--- app/controllers/bucket_lists_controller.rb
+++ app/controllers/bucket_lists_controller.rb
@@ -4,7 +4,7 @@ class BucketListsController < ApplicationController
     @destinations = Destination.all
     @activity = Activity.new
     # @activities = current_traveller.activities.includes(:destination).order(:priority)
-    @activities_objects = current_traveller.activities.all.to_json(:only=>[:latitude,:longitude, :name, :image_url])
+    @activities_objects = Activity.all.to_json(:only=>[:latitude,:longitude, :name, :image_url])
     @destination.activities.map{ |a| a.destination }
     @priorities = Activity.priorities
   end
